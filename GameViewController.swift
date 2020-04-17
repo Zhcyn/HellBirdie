d6766e480e9bb8dae42f6e6b33d6fe5d741419f5
Zhcyn <zhuoh_summer@163.com>
2020-04-17 19:10:20 +0800

    Add files via upload

---
+++ GameViewController.swift
@@ -0,0 +1,186 @@
+//
+//  GameViewController.swift
+//  Shapes
+//
+//  Created by Binh Huynh on 9/11/16.
+//  Copyright (c) 2016 Binh Huynh. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+import UIKit
+import Social
+import GameKit
+import StoreKit
+import AVFoundation
+//import GoogleMobileAds
+
+let KeyShare        = "CuBI.com.share"
+//let KeyLoadInterAds = "CuBI.com.loadInterAd"
+//let KeyShowInterAds = "CuBI.com.showInterAd"
+//let KeyLoadFB       = "CuBI.com.loadFB"
+let KeyRate         = "CuBI.com.rateUs"
+//let KeyBannerAds    = "CuBI.com.bannerAds"
+
+var musicPlaying    = true
+//var bannerDidLoad   = false
+var gameOverCount   = 0
+var speakerOn       = true
+
+class GameViewController: UIViewController, GKGameCenterControllerDelegate, UIAlertViewDelegate {
+    
+    var imageView = UIImageView()
+//    var interstitial: GADInterstitial!
+//    var rewardedInter: GADRewardBasedVideoAd!
+//    var banner: GADBannerView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        self.view.backgroundColor = .green
+        
+        if let view = self.view as! SKView? {
+            
+            let scene = GameScene(size:CGSize(width: 1242, height: 2208))
+            scene.scaleMode = .aspectFill
+            view.presentScene(scene)
+            view.ignoresSiblingOrder = true
+            view.showsFPS = false
+            view.showsNodeCount = false
+            view.showsPhysics = false
+        }
+        
+//        playBackgroundMusic(filename: "next_to_you.mp3")    
+        
+        //Authenticate Game Center
+        NotificationCenter.default.addObserver(self, selector:#selector(GameViewController.showAuthenticationViewController),name:NSNotification.Name(rawValue: PresentAuthenticationViewController), object: nil)
+        SGGameKit.sharedInstance.authenticateLocalPlayer()
+        
+        // Share
+        NotificationCenter.default.addObserver(self, selector: (#selector(GameViewController.displayShareSheet)), name:NSNotification.Name(rawValue: KeyShare), object: nil)
+        
+        // Pop Up Rate Us
+        NotificationCenter.default.addObserver(self, selector: (#selector(GameViewController.popRateUs)), name:NSNotification.Name(rawValue: KeyRate), object: nil)
+        
+        // Show Ads Banner
+//        NotificationCenter.default.addObserver(self, selector: (#selector(GameViewController.hideBanner)), name:NSNotification.Name(rawValue: KeyBannerAds), object: nil)
+
+    }
+    
+//    @objc func hideBanner() {
+//        if bannerDidLoad {
+//            print("Hidding Ads Banner")
+////            banner.removeFromSuperview()
+//        }
+//    }
+    
+//    func loadBanner() {
+////        banner = GADBannerView(adSize: kGADAdSizeSmartBannerPortrait)
+////        banner.adUnitID = "your_banner_id"
+////        banner.rootViewController = self
+////        let req: GADRequest = GADRequest()
+////        banner.load(req)
+////        banner.frame = CGRect(x: (view.bounds.width - banner.frame.width)/2, y: view.bounds.height - banner.frame.size.height, width: banner.frame.width, height: banner.frame.height)
+//    }
+    
+//    @objc func loadInterstitial() {
+//        interstitial = GADInterstitial(adUnitID: "your_interstitial_id")
+//        interstitial.delegate = self
+//        let request = GADRequest()
+//        interstitial.load(request)
+//    }
+    
+//    @objc func showInterstitial() {
+//        if interstitial.isReady {
+//            interstitial.present(fromRootViewController: self)
+//        }
+//    }
+    
+    @objc func displayShareSheet() {
+        let initialText = "OMG! Look at what I've got in Hyper Brick !\n" + "https://itunes.apple.com/us/app/id1508515544)"
+        if let myImage =  self.view?.pb_takeSnapshot() {
+            UIImageWriteToSavedPhotosAlbum(myImage, nil, nil, nil)
+            let activityViewController = UIActivityViewController(activityItems: [initialText,myImage as UIImage], applicationActivities: [])
+            activityViewController.excludedActivityTypes = [UIActivity.ActivityType.openInIBooks]
+            
+            if UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiom.phone {
+                self.present(activityViewController, animated: true, completion: {})
+            } else {
+//                let popup: UIPopoverController = UIPopoverController(contentViewController:activityViewController)
+//                popup.present(from: CGRect(self.view.frame.size.width / 2, self.view.frame.size.height / 4, 0, 0), in: self.view, permittedArrowDirections: UIPopoverArrowDirection.any, animated: true)
+            }
+        }
+    }
+    
+//    @objc func loadFBPage() {
+//        let url = "https://www.facebook.com/AppBoo-257026071346825/"
+//        UIApplication.shared.openURL(NSURL(string: url)! as URL)
+//    }
+    
+    @objc func popRateUs(){
+        let alert = UIAlertController(title: "Enjoying HYPER BRICK ?", message: "Write a review to let us know your thoughts", preferredStyle: UIAlertController.Style.alert)
+        self.present(alert, animated: true, completion: nil)
+        alert.addAction(UIAlertAction(title: "Cancel", style: .default, handler: { action in
+            switch action.style{
+            case .default:
+                print("closed", terminator: "")
+            case .cancel:
+                print("cancel")
+            case .destructive:
+                print("destructive")
+            }
+        }))
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+            switch action.style{
+            case .default:
+                let appId = "1508515544"
+                if let url = NSURL(string: "itms-apps://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?type=Purple+Software&id=\(appId)") {
+                    UIApplication.shared.openURL(url as URL)
+                }
+                print("closed", terminator: "")
+            case .cancel:
+                print("cancel")
+            case .destructive:
+                print("destructive")
+            }
+        }))
+    }
+    
+    @objc func showAuthenticationViewController() {
+        let gameKitHelper = SGGameKit.sharedInstance
+        if let authenticationViewController = gameKitHelper.authenticationViewController {
+            self.present(authenticationViewController, animated: true,
+                         completion: nil)
+        }
+    }
+    
+    func gameCenterViewControllerDidFinish(_ gameCenterViewController: GKGameCenterViewController){
+        gameCenterViewController.dismiss(animated: true, completion: nil)
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+
+    override var shouldAutorotate : Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations : UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    override var prefersStatusBarHidden : Bool {
+        return true
+    }
+}
